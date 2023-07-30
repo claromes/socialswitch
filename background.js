4f52ed73bc3dd97aeca5a11bdb0519028feb6b4c
Claromes <claromes@hey.com>
2023-07-30 04:52:18 -0300

    version update, fix regex profile

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (tab.url && tab.url.startsWith("https://www.instagram.com/")) {
-    if (tab.url.match(/^https:\/\/www\.instagram\.com\/[^/]+\/$/)) {
+    if (tab.url.match(/^https:\/\/www\.instagram\.com\/[^/]+\/.*$/)) {
       const handle = tab.url.split("/")[3];
       const redirectUrl = `https://www.picuki.com/profile/${handle}/`;
       chrome.tabs.update(tabId, { url: redirectUrl });
